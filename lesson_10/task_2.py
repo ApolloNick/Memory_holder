59facaac2373d6921e61e485db93e2819999fa62
ApolloNick <nkostrov7@gmail.com>
2021-07-25 13:05:10 +0300

    changes_task_2_and_3

--- lesson_10/task_2.py
+++ lesson_10/task_2.py
@@ -1,10 +1,15 @@
+# def hide_email(email) -> str:
+#     splitted_email = email.split("@")
+#     first_part, second_part = list(splitted_email[0]), list(splitted_email[1])
+#     first_part[-1], first_part[-2], first_part[-3] = "*", "*", "*"
+#     second_part[0], second_part[1] = "*", "*"
+#     prepared_list = first_part + list("@") + second_part
+#     return "".join(prepared_list)
+#  Сделал в этом варианте, чтобы прятало именно первые 3 символа до @ и последующие 2, вне зависимости от строки
+
 def hide_email(email) -> str:
-    splitted_email = email.split("@")
-    first_part, second_part = list(splitted_email[0]), list(splitted_email[1])
-    first_part[-1], first_part[-2], first_part[-3] = "*", "*", "*"
-    second_part[0], second_part[1] = "*", "*"
-    prepared_list = first_part + list("@") + second_part
-    return "".join(prepared_list)
+    first_part, second_part = email.split("@")
+    return first_part[:3] + '***@**' + second_part[-4:]
 
 
-print(hide_email("hello@gmail.com"))
+print(hide_email("somebody_email@gmail.com"))
--- lesson_10/task_3.py
+++ lesson_10/task_3.py
@@ -1,5 +1,9 @@
 def longest_word(sequence_of_words):
-    return max(sequence_of_words.split(), key=len)
+    length_of_words = []
+    for word in sequence_of_words.split():
+        length_of_words.append((len(word), word))
+    length_of_words.sort()
+    print(length_of_words[-1][1])
 
 
-print(longest_word("What makes a good man"))
+longest_word("What makes a good man")
