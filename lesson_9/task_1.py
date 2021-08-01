8a9d26b0fd11f1d1d398ed5d2361f17eda286ea0
ApolloNick <nkostrov7@gmail.com>
2021-08-01 19:42:51 +0300

    refactoring

--- lesson_9/task_1.py
+++ lesson_9/task_1.py
@@ -1,5 +1,67 @@
 import argparse
 import csv
+import sys
+
+
+def preparing_args_for_name():
+    for key in list(args_dict):
+        value = args_dict[key]
+        if value is None or key == "o":
+            del args_dict[key]
+    return args_dict
+
+
+def check_args(row):
+    arguments_dict = {key: value for key, value in args_dict.items() if value is not None}
+    if not arguments_dict:
+        sys.exit(ValueError("You miss arguments for task"))
+    map_dict = {
+        "brand": "BRAND",
+        "color": "COLOR",
+        "year": "MAKE_YEAR",
+        "fuel": "FUEL",
+        "reg_num": "N_REG_NEW"
+    }
+    result = []
+    for key, value in arguments_dict.items():
+        if row[map_dict[key]] == value:
+            result.append(True)
+        else:
+            result.append(False)
+    return all(result)
+
+
+def create_filename():
+    current_list = []
+    title = preparing_args_for_name()
+    for key in title:
+        value = title[key]
+        if key == "o":
+            continue
+        else:
+            current_list.append(value)
+    args_for_name = "-".join(current_list)
+    return f"{args_for_name}.csv"
+
+
+def create_csv_file():
+    result = []
+    with open(args.o, "r+", encoding="utf-8") as File:
+        csv_reader = csv.DictReader(File, delimiter=";")
+        for row in csv_reader:
+            if check_args(row):
+                result.append(row)
+    write_csv(result)
+
+
+def write_csv(result):
+    fieldnames = ["D_REG", "BRAND", "MODEL", "COLOR", "MAKE_YEAR", "FUEL", "N_REG_NEW"]
+    with open(create_filename(), "w+", encoding="utf-8") as csv_file:
+        writer = csv.DictWriter(csv_file, delimiter=" ", fieldnames=fieldnames,
+                                extrasaction="ignore")
+        writer.writeheader()
+        for row in result:
+            writer.writerow(row)
 
 
 if __name__ == '__main__':
@@ -12,55 +74,8 @@ if __name__ == '__main__':
     parser.add_argument("--fuel")
     parser.add_argument("--reg_num")
     args = parser.parse_args()
-
-
-def reading_csv_data():
-    whole_data = [["D_REG", "BRAND", "MODEL", "COLOR", "MAKE_YEAR", "FUEL", "N_REG_NEW"]]
-    with open("tz_opendata_z01012021_po01072021.csv", "r", encoding="utf-8") as csv_file:
-        reading_csv = csv.DictReader(csv_file, delimiter=";")
-        for row in reading_csv:
-            prepared_list = []
-            if row["BRAND"] == args.brand:
-                if row["COLOR"] == args.color:
-                    if row["MAKE_YEAR"] == args.year:
-                        prepared_list.append(row["D_REG"])
-                        prepared_list.append(row["BRAND"])
-                        prepared_list.append(row["MODEL"])
-                        prepared_list.append(row["COLOR"])
-                        prepared_list.append(row["MAKE_YEAR"])
-                        prepared_list.append(row["FUEL"])
-                        prepared_list.append(row["N_REG_NEW"])
-                        whole_data.append(prepared_list)
-    return whole_data
-
-
-def corrected_name():
-    prepared_string = []
-    if args.brand != "None":
-        prepared_string.append(args.brand)
-        if args.color != "None":
-            prepared_string.append(args.color)
-            if args.year != "None":
-                prepared_string.append(args.year)
-                if args.fuel != "None":
-                    prepared_string.append(args.fuel)
-                    if args.reg_num != "None":
-                        prepared_string.append(args.reg_num)
-                    else:
-                        print(None)
-    verified_name = "-".join(prepared_string)
-    return verified_name
-
-
-def writing_csv(name_file):
-    list_of_data = reading_csv_data()
-    with open(f"{name_file}.csv", "w", encoding="utf-8") as file:
-        writing_csv = csv.writer(file)
-        writing_csv.writerows(list_of_data)
-
-
-writing_csv(corrected_name())
-
-
+    args_dict = {"brand": args.brand, "color": args.color, "year": args.year,
+                 "fuel": args.fuel, "reg_num": args.reg_num}
+    create_csv_file()
 
 
