12df12fb4e467efd2becf44fb0771f37014c8be7
ApolloNick <nkostrov7@gmail.com>
2021-08-22 22:20:31 +0300

    airport_updated

--- lesson_16/airport.py
+++ lesson_16/airport.py
@@ -4,69 +4,62 @@ from re import search
 
 
 class CheckDataAirport:
-    def __init__(self):
-        self.ident_code = args.ident
-        self.country = args.country
-        self.name = args.name
+    def __init__(self, iata_code=None, country=None, name=None):
+        self.data = self.load_data()
+        self.iata_code = iata_code
+        self.country = country
+        self.name = name
 
-    def check_ident_code(self):
+    def load_data(self):
         with open("airport-codes_csv.csv", "r", encoding="utf-8") as file:
             csv_reader = csv.DictReader(file)
-            ident_code = {}
-            for line in csv_reader:
-                if line["ident"] == self.ident_code:
-                    try:
-                        ident_code.update(line)
-                    except AirportNotFoundError:
-                        print(AirportNotFoundError(self.ident_code, message="Airport not found"))
-            return ident_code
+            return [line for line in csv_reader]
+
+    def check_iata_data(self):
+        for line in self.data:
+            if line["iata_code"] == self.iata_code.upper():
+                return line
+        raise AirportNotFoundError(self.iata_code, message="Airport not found")
 
     def check_country(self):
-        with open("airport-codes_csv.csv", "r", encoding="utf-8") as file:
-            csv_reader = csv.DictReader(file)
-            dict_of_airports = {}
-            for line in csv_reader:
-                if line['iso_country'] == self.country:
-                    try:
-                        dict_of_airports.update(line)
-                    except Exception:
-                        raise CountryNotFoundError(self.country, message="Country not found")
-            return dict_of_airports
+        country_airports = []
+        for line in self.data:
+            if line['iso_country'] == self.country:
+                country_airports.append(line)
+        if not country_airports:
+            raise CountryNotFoundError(self.country, message="Country not found")
+        return country_airports
 
     def check_name_of_airport(self):
-        with open("airport-codes_csv.csv", "r", encoding="utf-8") as file:
-            csv_reader = csv.DictReader(file)
-            list_of_names = []
-            for line in csv_reader:
-                if search(self.name, line['name']):
-                    try:
-                        list_of_names.append(line['name'])
-                    except Exception:
-                        raise AirportNotFoundError(self.name, message="Airport not found")
-            return list_of_names
+        list_of_names = []
+        for line in self.data:
+            if search(self.name, line['name']):
+                list_of_names.append(line['name'])
+        if not list_of_names:
+            raise AirportNotFoundError(self.name, message="Airport not found")
+        return list_of_names
 
-    def check_validation_ident_code(self):
-        valid_code = self.ident_code.upper()
-        if valid_code == self.ident_code:
+    def check_validation_iata_code(self):
+        valid_code = self.iata_code.upper()
+        if valid_code == self.iata_code:
             print("Valid code")
         else:
             raise IATACodeError
 
-    def check_amount_of_params(self):
-        if len(vars(first_try)) < 0:
-            raise NoOptionsFoundError(message="Parameters not found")
-        elif len(vars(first_try)) > 1:
-            raise MultipleOptionsError(message="Just one parameter is necessary")
-        else:
-            print("Your amount of params is corrected")
+
+def check_amount_of_params():
+    if len([row for row in vars(args).values() if row is not None]) > 1:
+        raise MultipleOptionsError(message="Just one parameter is necessary")
+    if not len([row for row in vars(args).values() if row is not None]):
+        raise NoOptionsFoundError(message="Parameters not found")
 
 
 class AirportNotFoundError(Exception):
     """Exception raised for errors in the input ident_code or name error."""
 
-    def __init__(self, ident_code: str, message="Airport not found"):
-        self.ident_code = ident_code
-        self.message = message
+    def __init__(self, iata_data: str, message="Airport not found"):
+        self.iata_data = iata_data
+        self.message = f"{iata_data} - {message}"
         super().__init__(self.message)
 
 
@@ -75,46 +68,43 @@ class CountryNotFoundError(Exception):
     """Exception raised for errors for airport location (country)"""
     def __init__(self, country, message="Country not found"):
         self.country = country
-        self.message = message
+        self.message = f"{country} - {message}"
         super().__init__(self.message)
 
 
 class MultipleOptionsError(Exception):
-    def __init__(self, numbers_of_params, message="Just one parameter is necessary"):
-        self.numbers_of_params = numbers_of_params
+    def __init__(self, message="Just one parameter is necessary"):
         self.message = message
         super().__init__(self.message)
 
 
 class NoOptionsFoundError(Exception):
-    def __init__(self, numbers_of_params, message="Parameters not found"):
-        self.numbers_of_params = numbers_of_params
+    def __init__(self, message="Parameters not found"):
         self.message = message
         super().__init__(self.message)
 
 
 class IATACodeError(Exception):
-    def __init__(self, ident_code, message="Not valid value"):
-        self.ident_code = ident_code
-        self.message = message
+    def __init__(self, iata_code, message="Not valid value"):
+        self.iata_code = iata_code
+        self.message = f"{iata_code} - {message}"
         super().__init__(self.message)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Checking data from airport")
-    # parser.add_argument("--iata_data", help="Three capital letter") это поле пустое в документе
-    parser.add_argument("--ident", default="00AA", help="Ident code in airport doc")
-    parser.add_argument("--country", default="US", help="Name of a country")
-    parser.add_argument("--name", default="liman", help="Name of airport")
+    parser.add_argument("--iata_code", help="Three capital letter")
+    parser.add_argument("--country", help="Name of a country")
+    parser.add_argument("--name", help="Name of airport")
     args = parser.parse_args()
+    check_amount_of_params()
+    first_try = CheckDataAirport(name=args.name, country=args.country, iata_code=args.iata_code)
 
 
-first_try = CheckDataAirport()
-print(first_try.check_ident_code())
+print(first_try.check_iata_data())
 # print(first_try.check_country())
 # print(first_try.check_name_of_airport())
-# first_try.check_validation_ident_code()
-# first_try.check_amount_of_params()
+first_try.check_validation_iata_code()
 
 
 
